aa2754224fcc399aad7ac06003c40413de975d84
vezz <senszalonegoprogramisty@wp.pl>
2019-01-28 16:34:02 +0100

    working menu, some scanner bug

--- src/fractionCalculator/Main.java
+++ src/fractionCalculator/Main.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
-    private static Fraction fraction = new Fraction();
+    private static final String OPTIONS = "+-*/=Q";
+
     public static void main(String[] args) {
 
-        System.out.println("This is fraction calculator, you can add, subtract, multiply and divide fractions.\n" +
-                "Please enter +, -, *, / or Q to quit");
+        System.out.println("This is fraction calculator, you can add, subtract, multiply and divide fractions.\n");
 
-        String menu = scanner.nextLine();
         boolean flag = true;
-
+        String menu;
         while (flag) {
+            menu = getOperation();
             switch (menu) {
                 case "Q":
                     System.out.println("Quiting program");
@@ -23,35 +23,100 @@ public class Main {
                 case "+":
                     addFractions();
                     break;
+                case "-":
+                    subtractFractions();
+                    break;
+                case "*":
+                    multiplyFractions();
+                    break;
+                case "/":
+                    divideFractions();
+                    break;
+                case "=":
+                    isFractionEqual();
+                    break;
             }
         }
     }
 
-    public static String getOperation(Scanner input) {
-        System.out.println("Please add a fraction a/b or a");
-        //Asks the user to enter in a valid mathematical
-        //operation. If the user enters anything except
-        // "+", "-", "/", "*", "=", "q", or "Q" it should
-        // re-prompt them until there is valid input.
-        return "";
+    public static String getOperation() {
+        System.out.println("Please enter +, -, *, /, = or Q to quit");
+        String menu = scanner.nextLine();
+        while (!OPTIONS.contains(menu)) {
+            System.out.println("Invalid input. Please enter +, -, *, /, = or Q to quit");
+            menu = scanner.nextLine();
+        }
+        return menu;
     }
 
-    public static boolean validFraction(String input) {
+    private static Fraction getFraction(Scanner input) {
+        String fractionStr = input.next();
+
+        while (!fractionStr.matches("-?\\d+/*(\\+?[1-9]\\d*)*")) {
+            System.out.print("Invalid fraction. Please enter (a/b) or (a): ");
+            fractionStr = scanner.next();
+        }
+        if (fractionStr.contains("/")) {
+            String[] splitFraction = fractionStr.split("/");
+            return new Fraction(Integer.parseInt(splitFraction[0]), Integer.parseInt(splitFraction[1]));
+        } else {
+            return new Fraction(Integer.parseInt(fractionStr));
+        }
+    }
+
+    private static void addFractions() {
+        System.out.println("Please enter fraction one");
+        Fraction fraction1 = getFraction(scanner);
+        System.out.println("Please enter fraction two");
+        Fraction fraction2 = getFraction(scanner);
+
+        System.out.println(fraction1.toString()+" + "+ fraction2.toString()+" = "+fraction1.add(fraction2));
+    }
+
+    private static void subtractFractions() {
+        System.out.println("Please enter fraction one");
+        Fraction fraction1 = getFraction(scanner);
+        System.out.println("Please enter fraction two");
+        Fraction fraction2 = getFraction(scanner);
+
+        System.out.println(fraction1.toString()+" - "+ fraction2.toString()+" = "+fraction1.subtract(fraction2));
+    }
+
+    private static void multiplyFractions() {
+        System.out.println("Please enter fraction one");
+        Fraction fraction1 = getFraction(scanner);
+        System.out.println("Please enter fraction two");
+        Fraction fraction2 = getFraction(scanner);
+
+        System.out.println(fraction1.toString()+" * "+ fraction2.toString()+" = "+fraction1.multiply(fraction2));
+
+    }
+
+    private static void divideFractions() {
+        System.out.println("Please enter fraction one");
+        Fraction fraction1 = getFraction(scanner);
+        System.out.println("Please enter fraction two");
+        Fraction fraction2 = getFraction(scanner);
+
+        System.out.println(fraction1.toString()+" / "+ fraction2.toString()+" = "+fraction1.divide(fraction2));
+    }
+
+    private static void isFractionEqual() {
+        System.out.println("Please enter fraction one");
+        Fraction fraction1 = getFraction(scanner);
+        System.out.println("Please enter fraction two");
+        Fraction fraction2 = getFraction(scanner);
+
+        System.out.println("Are fractions equal? "+fraction1.equals(fraction2));
+    }
+
+}
+
+/*
+    public static boolean validFraction(String fraction) {
+
         //returns true if the parameter is in the
         // form "a/b" where a is any int and b is any positive int
         return false;
     }
-
-    public static Fraction getFraction1(Scanner input) {
-
-        //It prompts the user for a String that is a validFraction.
-        // If they enter any thing that is not a valid Fraction, it
-        // should re-prompt them until it is valid
-
-        return null;
-    }
-
-    public static void addFractions() {
-        
-    }
-}
+*/
